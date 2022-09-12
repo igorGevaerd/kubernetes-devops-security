a3b1d9fe81fdb2238e463ffc99fce1cd0ce369e3
igorGevaerd <igor.gevaerd@gmail.com>
2022-09-12 11:26:50 -0300

    fixed 'undo'

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -5,7 +5,7 @@ sleep 60s
 # if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"*]]; then
 if [[ 'viadino' != *"successfully rolled out"* ]]; then
     echo "Deployment ${deploymentName} Rollout has Failed"
-    kubectl -n default rollout undu deploy ${deploymentName}
+    kubectl -n default rollout undo deploy ${deploymentName}
     exit 1;
 else
     echo "Deployment ${deploymentName} Rollout is Success"
