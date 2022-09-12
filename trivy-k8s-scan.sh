b86dd65ba8f50e26e0a9e23998a5263a6f9a68ab
igorGevaerd <igor.gevaerd@gmail.com>
2022-09-12 18:57:13 -0300

    added exit 1

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -10,6 +10,7 @@ echo "Exit Code: ${exit_code}"
 
 if [[ ${exit_code} == 1 ]]; then
     echo "Image scanning failed. Vulnerabilities found"
+    exit 1
 else
     echo "Image scanning passed. No Vulnerabilities found"
 fi
